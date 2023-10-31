fde85c6acca3ec4ab3712a718267ddad92581036
ASUS <ASUS@DESKTOP-UEHIM0J>
2023-10-31 22:06:45 +0330

    Add project files.

---
+++ factoriel2/Program.cs
@@ -0,0 +1,22 @@
+﻿//shomare 5  factoriel
+//amir mahdi hosein njad
+string YESorNO;
+do
+{
+    Console.WriteLine("adad mored nazar ra baray factoriel vared konid ");
+
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    long f = number;
+
+    if (number == 0)
+        f = 1;
+    for (int i = 1; i < number; i++)
+
+    {
+        f = f * i;
+    }
+    Console.WriteLine(f);
+    Console.WriteLine("mikhay adad dige ham  test koni?y or n");
+} while (Console.ReadLine() == "y");
+Console.WriteLine("------payan------");
